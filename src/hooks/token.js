eb8b762c7a68a5023d1da1211bd1e1a8a3dc0347
Blake Eriks <eblake98@vt.edu>
2021-11-22 18:56:48 -1000

    Attempt at react-query for tokens

---
+++ src/hooks/token.js
@@ -0,0 +1,23 @@
+import { useQuery } from "react-query";
+import useHttp from "./http";
+ 
+const API_BASE_URL = process.env.REACT_APP_API_URL
+
+const useTokenPrices = () => {
+    const http = useHttp()
+    return useQuery('tokens/prices', () => () => http.get(`${API_BASE_URL}/tokens/prices`))
+}
+
+const useTokenPrice = symbol => {
+    const http = useHttp()
+    return useQuery(`tokens/prices/${symbol}`, () => http.get(`${API_BASE_URL}/tokens/prices/${symbol}`), {enabled: !!symbol})
+}
+
+const useTokenHistory = (symbol, period) => {
+    const http = useHttp()
+    return useQuery(`tokens/history/${symbol}/${period}`, async () => {
+        return await http.get(`${API_BASE_URL}/tokens/history/${symbol}/${period}`)
+    }, {enabled: !!symbol && !!period})
+}
+
+export { useTokenPrice, useTokenPrices, useTokenHistory };
