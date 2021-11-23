6224fccc72735f23a49aa5fb7036751785f25777
Blake Eriks <eblake98@vt.edu>
2021-11-22 22:04:16 -1000

    Create signup view that mocks login

---
+++ src/component/Signup.js
@@ -0,0 +1,44 @@
+import { useState } from "react"
+import { useAuthState } from "../hooks/auth"
+import useUserActions from "../hooks/user"
+ 
+const Signup = () => { 
+    
+    const userActions = useUserActions()
+    const [form, setForm] = useState({username: '', password: ''})
+    const [badLogin, setBadLogin] = useState(false)
+    const [auth] = useAuthState()
+
+    const handleSubmit = async event => {
+        event.preventDefault()
+        try {
+            await userActions.login({...form})
+        }
+        catch (err) {
+            setBadLogin(true)
+        }
+    }
+
+    const onChange = event => {
+        setForm({...form, [event.target.name]: event.target.value})
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit} action="" method="post">
+                <div>
+                    <input type="text" name="username" value={form.username} onChange={onChange} placeholder="username" required></input>
+                </div>
+                <div>
+                    <input type="password" name="password" value={form.password} onChange={onChange} placeholder="password" required></input>
+                </div>
+
+                <div>
+                    <button type="submit" value="Login">Login</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Signup
