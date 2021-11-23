725daf5429be14b2171b49fbc714c97cd64a5d32
AlexTakada <alex_takada@hotmail.com>
2021-11-22 20:34:13 -0800

    some styling and started login

---
+++ src/styles/Button.js
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+const Button = styled.button`
+    cursor: pointer;
+    font-family: emoji;
+    border: none;
+`
+
+
+export const LoginButton = styled(Button)`
+    border: none;
+    outline: none;
+    padding: 0.8em;
+    border-radius: 18px;
+    background: #7c7c7c2b;
+    color: white;
+    `
