6e90191f40c8d361a3a12ef11036e3dc911fe720
Blake Eriks <eblake98@vt.edu>
2021-11-22 22:03:44 -1000

    Fix errors and remove classNames

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,12 +1,11 @@
 import { useState } from "react"
-import { Link } from "react-router-dom"
+import useUserActions from "../hooks/user"
  
 const Login = () => { 
     
     const userActions = useUserActions()
     const [form, setForm] = useState({username: '', password: ''})
     const [badLogin, setBadLogin] = useState(false)
-    const [auth] = useAuthState()
 
     const handleSubmit = async event => {
         event.preventDefault()
@@ -23,26 +22,19 @@ const Login = () => {
     }
 
     return (
-        <div class="card">
+        <div>
             <form onSubmit={handleSubmit} action="" method="post">
-                
-                <div className="container">
-                        <Input type="text" name="username" value={form.username} onChange={onChange} placeholder="username" required></Input>
-                    </div>
-
-                    <div className="container">
-                        <Input type="password" name="password" value={form.password} onChange={onChange} placeholder="password" required></Input>
-                    </div>
-
-                    <div className="container">
-                        <LoginButton type="submit" value="Login">Login</LoginButton>
-                    </div>
-
+                <div>
+                    <input type="text" name="username" value={form.username} onChange={onChange} placeholder="username" required></input>
+                </div>
+                <div>
+                    <input type="password" name="password" value={form.password} onChange={onChange} placeholder="password" required></input>
+                </div>
+                <div>
+                    <button type="submit" value="Login">Login</button>
+                </div>
             </form>
-            
         </div>
-    
-
     )
 }
 
