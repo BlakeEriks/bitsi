754fda8c567f6b9bed23a8218977cc5ee14eefa8
Blake Eriks <eblake98@vt.edu>
2021-11-26 19:48:32 -1000

    Add movers styles

--- src/styles/SidePanel.js
+++ src/styles/SidePanel.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { VerticalFlexBox } from "./Boxes";
 import { colors } from "./Colors";
 
 export const BalanceText = styled.div`
@@ -6,6 +7,25 @@ export const BalanceText = styled.div`
   font-size: 2em;
   border-bottom: 1px solid ${colors.gray};
 `;
+
+export const MoversContainer = styled(VerticalFlexBox)`
+    width: 80%;
+    margin: 20px auto;
+`
+
+export const Mover = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-size: 2ch;
+    cursor: pointer;
+`
+
+export const PercentChange = styled.span`
+    color: ${props => props.positive ? 'green' : 'red'};
+    font-size: 1.3ch;
+`
 
 export const TradeContainer = styled.div`
   display: flex;
@@ -30,3 +50,11 @@ export const TradeInput = styled.input`
   margin: 5px 0 10px 0;
   width: 90%;
 `;
+
+export const SubHeader  = styled.div`
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    font-size: 24px;
+    color: ${colors.light};
+`
