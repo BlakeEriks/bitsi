3cd8950d551ab85a81d9cd0a11cbb01679725acb
Blake Eriks <eblake98@vt.edu>
2021-12-06 15:04:57 -0800

    Move components from sidepanel into dashboard

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -1,10 +1,13 @@
 import { useViewState } from "../hooks/view"
-import { Card, DashboardContainer, DashboardLeft } from "../styles/Boxes"
+import { Card, DashboardContainer, VerticalFlexBox } from "../styles/Boxes"
 import Assets from "./Assets"
+import Balance from "./Balance"
+import BiggestMovers from "./BiggestMovers"
 import Chart from "./Chart"
 import Leaderboard from "./Leaderboard"
 import Login from "./Login"
 import Signup from "./Signup"
+import Trade from "./Trade"
 
 const Dashboard = () => {
 
@@ -12,14 +15,19 @@ const Dashboard = () => {
 
     return ( 
         <DashboardContainer>
-            <DashboardLeft>
+            <VerticalFlexBox>
+                <Balance />
+                <BiggestMovers />
+                <Trade />
+            </VerticalFlexBox>
+            <VerticalFlexBox>
                 <Card>
                     {viewState === 'dashboard' && <Chart />}
                     {viewState === 'login' && <Login />}
                     {viewState === 'signup' && <Signup />}
                 </Card>
                 <Assets />
-            </DashboardLeft>
+            </VerticalFlexBox>
             <Leaderboard />
         </DashboardContainer>
     )
