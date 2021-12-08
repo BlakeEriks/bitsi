7ff0778d15af20cadb28aa78cad443067a531472
Blake Eriks <eblake98@vt.edu>
2021-12-08 14:28:02 -0800

    Add spinner

--- src/component/LineChart.js
+++ src/component/LineChart.js
@@ -1,13 +1,14 @@
-import { ResponsiveLine } from "@nivo/line"
-import moment from "moment"
-import useChartData from "../hooks/chartData"
-import { ToolTipContainer, ToolTipMainText, ToolTipSubText } from "../styles/Chart"
-import { colors } from "../styles/Colors"
-import { toDollarFormat } from "../util/dollarUtil"
+import { ResponsiveLine } from "@nivo/line";
+import moment from "moment";
+import Loader from "react-loader-spinner";
+import useChartData from "../hooks/chartData";
+import { ToolTipContainer, ToolTipMainText, ToolTipSubText } from "../styles/Chart";
+import { colors } from "../styles/Colors";
+import { toDollarFormat } from "../util/dollarUtil";
 
 const LineChart = ({period}) => {
 
-    const {isSuccess, chartConfig, chartData} = useChartData(period)
+    const {isFetching, chartConfig, chartData} = useChartData(period)
 
     const theme = {
         axis: {
@@ -20,7 +21,7 @@ const LineChart = ({period}) => {
         },
         crosshair: {
             line: {
-                stroke: colors.gray,
+                stroke: colors.dark,
                 strokeWidth: 1,
                 strokeOpacity: 0.35,
             },
@@ -41,30 +42,43 @@ const LineChart = ({period}) => {
     }
 
     return (
-        chartData &&
-        <ResponsiveLine
-            data={chartData}
-            colors={colors.accent}
-            margin={{ top: 20, right: 20, bottom: 40, left: 20 }}
-            xScale={{ type: 'time'}}
-            enableGridX={false}
-            yScale={{ type: 'linear', min: 'auto', max: 'auto' }}
-            enableGridY={false}
-            enableSlices={'x'}
-            sliceTooltip={Tooltip}
-            axisRight={null}
-            axisLeft={null}
-            layers={[ 'axes', 'areas', 'lines', 'points',  'mesh', 'crosshair', 'slices']}
-            animate={true}
-            axisBottom={{
-                orient: 'bottom',
-                tickSize: 0,
-                tickPadding: 15,
-                ...chartConfig?.axisBottom
-            }}
-            pointSymbol={() => <g><circle r={0}/></g>}
-            theme={theme}
-        />
+        <>
+            {isFetching && 
+            <div style={{height: '0', width: '95%', margin: '0 auto'}}>
+                <div style={{position: 'relative', height: '17.6em', display: 'flex', alignItems: 'center', justifyContent: 'center', zIndex:2}}>
+                    <Loader 
+                    type="ThreeDots"
+                    color={colors.gray}
+                    height={100}
+                    width={100} 
+                    />
+                </div>
+            </div>}
+            {chartData &&
+            <ResponsiveLine
+                data={chartData}
+                colors={isFetching ? 'rgba(238, 108, 138, 0.5)' : colors.accent}
+                margin={{ top: 20, right: 20, bottom: 40, left: 20 }}
+                xScale={{ type: 'time'}}
+                enableGridX={false}
+                yScale={{ type: 'linear', min: 'auto', max: 'auto' }}
+                enableGridY={false}
+                enableSlices={'x'}
+                sliceTooltip={Tooltip}
+                axisRight={null}
+                axisLeft={null}
+                layers={[ 'axes', 'areas', 'lines', 'points',  'mesh', 'crosshair', 'slices']}
+                animate={true}
+                axisBottom={{
+                    orient: 'bottom',
+                    tickSize: 0,
+                    tickPadding: 15,
+                    ...chartConfig?.axisBottom
+                }}
+                pointSymbol={() => <g><circle r={0}/></g>}
+                theme={theme}
+            />}
+        </>
     )
 }
 
