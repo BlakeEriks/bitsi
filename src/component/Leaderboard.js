68294c6e8925e3570ac529e7c7b1c6774bb9608a
Blake Eriks <eblake98@vt.edu>
2021-12-09 17:21:23 -0800

    Highlight user on leaderboard

--- src/component/Leaderboard.js
+++ src/component/Leaderboard.js
@@ -1,3 +1,4 @@
+import { useAuthState } from "../hooks/auth"
 import { useChartState } from "../hooks/chartState"
 import { usePortfolios } from "../hooks/portfolio"
 import { Card, VerticalFlexBox } from "../styles/Boxes"
@@ -10,6 +11,7 @@ const Leaderboard = () => {
     
     const leaderboard = usePortfolios()
     const [chartState, setChartState] = useChartState()
+    const [auth] = useAuthState()
 
     return (
         <Card width='10em'>
@@ -18,7 +20,7 @@ const Leaderboard = () => {
             </SubHeader>
             <VerticalFlexBox>
                 {leaderboard?.map( (user, index) => 
-                    <Entry key={index} onClick={() => setChartState({mode: 'portfolio', username: user.username})}>
+                    <Entry key={index} onClick={() => setChartState({mode: 'portfolio', username: user.username})} highlight={user.username === auth?.username}>
                         <NameText>{index+1 + '.' + ' ' + user.username}</NameText>
                         <InfoText>{shortenDollar(user.value)}</InfoText>
                     </Entry>
