6f5e2617c30c023f49a1dcbe04fa454c007b06d9
Blake Eriks <eblake98@vt.edu>
2021-12-09 17:07:25 -0800

    Make assets clickable

--- src/component/Asset.js
+++ src/component/Asset.js
@@ -1,3 +1,4 @@
+import { useChartState } from "../hooks/chartState"
 import { useTokens } from "../hooks/token"
 import { AssetContainer } from "../styles/Assets"
 import { shortenQuantity, toDollarFormat } from "../util/dollarUtil"
@@ -6,6 +7,7 @@ import { shortenQuantity, toDollarFormat } from "../util/dollarUtil"
 const Asset = ({symbol, quantity}) => {
 
     const {tokens} = useTokens()
+    const [chartState, setChartState] = useChartState()
 
     const getValue = (symbol, quantity) => {
         const token = tokens?.find(token => token.symbol === symbol)
@@ -15,7 +17,7 @@ const Asset = ({symbol, quantity}) => {
     }
 
     return (
-        <AssetContainer>
+        <AssetContainer onClick={() => setChartState({mode: 'token', token: symbol})}>
             <div className='quantity'>
                 {shortenQuantity(quantity) + ' ' + symbol}
             </div>
